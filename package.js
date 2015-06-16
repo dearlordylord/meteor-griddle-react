53d8305795efd613c69477eb72bcb0aaca20054b
Igor Loskutov <igor.loskutoff@gmail.com>
2015-06-16 18:46:50 +0300

    deprecation warning

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'firfi:griddle-react',
-  version: '0.0.3',
+  version: '0.0.4',
   // Brief, one-line summary of the package.
   summary: 'ReactJS Griddle port for Meteor',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/Firfi/meteor-griddle-react',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: 'METEOR.md'
 });
 
 Package.onUse(function(api) {
